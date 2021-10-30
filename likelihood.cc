89b1006081583278cf9cc92d1526f61662b025ac
Konver24 <>
2021-10-30 11:18:09 +0000

    final

--- likelihood.cc
+++ likelihood.cc
@@ -4,11 +4,13 @@
 #include <cmath>
 #include <algorithm>
 
+//Function to calculate Poisson probability
 double poisson(double mu, int k) {
     double poisson_value = (pow (mu, k) * exp(-mu)) / tgamma(k + 1);
     return poisson_value;
 }
 
+//Funtcion to calculate the likelihood
 double prob(std::vector<int> daten, double mu){
   double likelihood = 1;
   for(int k : daten){
@@ -17,6 +19,7 @@ double prob(std::vector<int> daten, double mu){
   return likelihood;
 }
 
+//Function to calculate the likelihood(k_i)
 double prob_k(std::vector<int> daten){
   double likelihood = 1;
   for(int k: daten){
@@ -28,11 +31,18 @@ double prob_k(std::vector<int> daten){
 int main() {
     using namespace std;
     vector<int> daten;
-    double mu = 3.11538;
-    double log_likelihood_delta_min = 1e6;        //random start value
-    double mu_min = 1;        //random start value
-    vector<double> interval;
+    double mu = 3.11538;                      //mean of datensumme.txt
+    double likelihood_max = 0;                //random start value
+    double mu_max = 1;                        //random start value
+    double log_likelihood_delta_min = 1e6;    //random start value
+    double mu_min = 1;                        //random start value
+    double lambda;                            //likelihood ratio
+    double log_lambda;                        //-2*log(lambda)
+    double n_dof;                             //degrees of freedom/mean of Chi^2 square distribution
+    double standard_dev_chi_sq;              //standard deviation 
+    double rel_dev_chi_sq;                   //relative deviation
 
+    //read numbers from datensumme.txt and save them a vector
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
@@ -41,8 +51,11 @@ int main() {
     }
     fin.close();
 
-    cout << "L(mu = 3.11538)= " << prob(daten, mu) << std::endl;
-
+    //write L(mu) - mu from 0 to 6 - to likelihood.txt
+    //find Maximum of L(mu)
+    //write -2*log(L(mu)) - mu from 0 to 6 - to nll.txt
+    //write -2*log(L(mu))-(-2*log(3.11538))
+    //find minimum of this(^) function 
     ofstream fout_like("likelihood.txt");
     ofstream fout_log_like("nll.txt");
     ofstream fout_log_like_delta("deltanll.txt");
@@ -54,22 +67,41 @@ int main() {
       fout_like << j << " " << likelihood_value << std::endl;
       fout_log_like << j << " " << log_likelihood_value << std::endl;
       fout_log_like_delta << j << " " << log_likelihood_delta_value << std::endl;
+
+      if (likelihood_value > likelihood_max){
+        mu_max = j;
+        likelihood_max = likelihood_value;
+      }
+
+      if (log_likelihood_delta_value < log_likelihood_delta_min){
+        mu_min = j;
+        log_likelihood_delta_min = log_likelihood_delta_value;
+      }
     }
     fout_like.close();
     fout_log_like.close();
     fout_log_like_delta.close();
 
-    double lambda = prob(daten,mu) / prob_k (daten);
-    double ratio_value = -2 * log(lambda);
+    //Calculation of lambda
+    lambda = prob(daten,mu) / prob_k (daten);
+    log_lambda = -2 * log(lambda);
 
-    cout << "-2 log (Lambda)= " << ratio_value << std::endl;
+    //Mean, standard deviation, relative deviation of Chi^2 distribution
+    n_dof = 233;
+    standard_dev_chi_sq = sqrt(2*n_dof);
+    rel_dev_chi_sq = (log_lambda - n_dof) / standard_dev_chi_sq;
 
-    double n_dof = 233;
-    double standard_dev_chi_sq = sqrt(2*n_dof);
-    double rel_dev_chi_sq = (ratio_value - n_dof) / standard_dev_chi_sq;
-
-    cout << "Chi^2:" << std::endl;
-    cout << "Mean = " << n_dof << std::endl;
-    cout << "Standard deviation= " << standard_dev_chi_sq << std::endl;
-    cout << "Relative deviation= " << rel_dev_chi_sq << std::endl;
+    //cout/print results
+    cout << "L(mu = 3.11538)= " << prob(daten, mu) << endl;
+    cout << "Maximum of likelihood:" << endl;
+    cout << "mu_max = " << mu_max << endl;
+    cout << "maximum value = " << likelihood_max << endl;
+    cout << "Minimum of deltanll.txt:" << endl;
+    cout << "mu_min = " << mu_min << endl;
+    cout << "minimum value = " << log_likelihood_delta_min << endl;
+    cout << "Chi^2:" << endl;
+    cout << "-2 log (Lambda)= " << log_lambda << endl;
+    cout << "Mean = " << n_dof << endl;
+    cout << "Standard deviation= " << standard_dev_chi_sq << endl;
+    cout << "Relative deviation= " << rel_dev_chi_sq << endl;
 }
--- poisson.cc
+++ poisson.cc
@@ -3,7 +3,7 @@
 #include <vector>
 #include <cmath>
 
-
+//Function to calculate Poisson probability
 double poisson(double mu, int k) {
     double poisson_value = (pow (mu, k) * exp(-mu)) / tgamma(k + 1);
     return poisson_value;
@@ -13,8 +13,9 @@ int main() {
     using namespace std;
     int N = 234;                //numbers in datensumme.txt
     double mu = 3.11538;        //mean of datensumme.txt
-    vector<int> zaehler(11);
+    vector<int> zaehler(11);    //counting vector
 
+    //read numbers from datensumme.txt and count how often the values k appear
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < N ; ++i) {
@@ -23,11 +24,12 @@ int main() {
     }
     fin.close();
 
+    //write count and expectation from the Poisson distribution to hist.txt
     ofstream fout("histpoi.txt");
     for(unsigned int k = 0; k < zaehler.size(); ++k){
-      cout << k << ": " << zaehler[k] << std::endl;
+      cout << k << ": " << zaehler[k] << endl;
       fout << k << " " << zaehler[k] << " " 
-      << N * poisson(mu, k) << std::endl;
+      << N * poisson(mu, k) << endl;
 
     }
     fout.close();
